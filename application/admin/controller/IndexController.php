9315a89c83328ab07962748b4310ae2b57bab940
lyb521569 <462966779@qq.com>
2017-04-12 00:30:18 +0800

    delete data
    delete data

--- application/admin/controller/IndexController.php
+++ application/admin/controller/IndexController.php
@@ -10,6 +10,22 @@ use app\common\model\Teacher;
 class IndexController extends Controller
 {
 
+
+	public function delete(){
+	// 获取要删除的对象
+		$Teacher = Teacher::get(17);
+
+        // 要删除的对象存在
+		if (!is_null($Teacher)) {
+            // 删除对象
+			if ($Teacher->delete()) {
+				return '删除成功';
+			}
+		}
+		return '删除失败';
+	}
+
+
 	public function insert(){
 
 // var_dump($_POST);
