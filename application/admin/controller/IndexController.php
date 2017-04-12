f5fbdc34cf982160fa0c533bb719eec79f290484
lyb521569 <462966779@qq.com>
2017-04-13 00:52:18 +0800

    upddate
    update

--- application/admin/controller/IndexController.php
+++ application/admin/controller/IndexController.php
@@ -11,31 +11,52 @@ class IndexController extends Controller
 {
 
 	public function update(){
-		// var_dump($_POST);
-		$teacher = new Teacher();
+
+		//获取请求的数据
 		$updateDate = Request::Instance()->post();
-		// var_dump($updateDate);
-		// var_dump($updateDate["name"]);
+		
+		//获取ID
+		$id = Request::Instance()->post('id');
+		if($id<=0){
+			return "系统错误，ID：".$id."不存在";
+		}
+
+		//获取模型
+		$teacher = Teacher::find($id);
+		//判断模型是否存在
+		if(is_null($teacher)){
+			return "要更新的数据不存在";
+		}
 		$teacher->name = $updateDate["name"];
 		$teacher->sex = $updateDate["sex"];
 		$teacher->username = $updateDate["username"];
 		$teacher->email = $updateDate["email"];
 		$teacher->id = $updateDate["id"];
-		try {
-			$res=$teacher->validate(true)->isupdate(true)->save();
-			var_dump($res);
-			if(!$res){
-				return "更新失败：".$teacher->getError();
-			}
-		} catch (\Exception $e) {
-			return "更新失败：".$e->getMessage();
+		$message="更新成功";
+		$res=$teacher->allowField(true)->validate(true)->isupdate(true)->save($teacher);
+		//更新数据是否成功		
+		if($res){
+			$this->success('更新成功',url('/admin'));
+		}else{
+			$message= "更新失败：".$teacher->getError();
+			return $message;
 		}
+		// 	try {
+		// 		$res=$teacher->validate(true)->isupdate(true)->save($teacher);
+		// 	// var_dump($res);
+		// 		if($res==0){
+		// 			$message= "更新失败：".$teacher->getError();
+		// 		}
+		// 	} catch (\Exception $e) {
+		// 		$message= "更新异常,：".$e->getMessage();
+		// 	}
+		// 	return $message;
+		// }else{
+  //       	 throw new \Exception("所更新的记录不存在", 1);   // 调用PHP内置类时，需要在前面加上 \ 
+
 	}
-
-
 	public function edit(){
-		// var_dump(Request::Instance()->param());
-		// $id = Request::instance()->param('id/d');
+		//要编辑的ID
 		$id = $this->request->param('id/d');
 		$teacher = Teacher::get($id);
 		if(is_null($teacher)){
@@ -44,7 +65,7 @@ class IndexController extends Controller
 		$this->assign('teacher',$teacher);
 		$html=$this->fetch();
 		return $html;	
-		
+
 
 	}
 
@@ -78,23 +99,32 @@ class IndexController extends Controller
     public function insert(){
 
 // var_dump($_POST);
-
-    	$postData = Request::Instance()->post();
-    	$teacher = new Teacher();
-    	$teacher->name = $postData['name'];
-    	$teacher->username = $postData["username"];
-    	$teacher->sex = $postData["sex"];
-    	$teacher->email = $postData["email"];
-		$teacher->create_time = $postData["create_time"];//database.php 配置开启自动时间戳
-		$status=$teacher->validate(true)->save();
-		// $status=$teacher->validate($teacher)->save($teacher->getData());
-		// var_dump($status);
-		if(false==$status){
-			return '新增失败'.$teacher->getError();
-		}else{
-			return "表单数据已提交到数据库,新增ID为：".$teacher->id;
-		}
-
+    	$message="新增失败";
+    	// try {
+    	var_dump(Request::Instance()->post());
+    	$postData =Request::Instance()->post();
+    	$Teacher = new Teacher();
+    	$Teacher->name = $postData['name'];
+    	$Teacher->username = $postData["username"];
+    	$Teacher->sex = $postData["sex"];
+    	$Teacher->email = $postData["email"];
+				$Teacher->create_time = $postData["create_time"];//database.php 配置开启自动时间戳
+				// $status=$Teacher->Validate(true)->save($Teacher);//这样写 报异常：fields not exists:[connection]，还没搞明白
+				$status=$Teacher->Validate(true)->save();
+					// $status=$Teacher->validate($Teacher)->save($Teacher->getData());
+				var_dump($status);
+				if($status==1){
+					return $this->success("用户 ".$Teacher->name."新增成功。",url('index'));
+				}else{
+					$message ='新增失败'.$Teacher->getError();
+				}
+			// } catch (\think\Exception\HttpResponseException $e) {
+			// 	return "新增失败，异常信息：".$e->getMessage();
+			// }
+			// catch (\Exception $e) {
+			// 	return "异常信息：".$e->getMessage();
+			// }
+			// return $this->error($message);
 		//①
 		// $teacher = array();
 		// $teacher['name']="利息那生";
@@ -106,7 +136,7 @@ class IndexController extends Controller
 		// return $teacher['name']."成功插入数据库。";
 		// var_dump($state);
 		// 
-		
+
 		//②
 		// $teacher = new Teacher();
 		// $teacher->name = "李元霸";
@@ -116,37 +146,43 @@ class IndexController extends Controller
 		// $teacher->save();
 		// return "新增ID为：".$teacher->id."的数据到数据库";
 
-	}
+			}
 
 
-	public function add(){
-		$html=$this->fetch();
-		return $html; 
+			public function add(){
+				try {
+					$html=$this->fetch();
+					return $html; 
+				} catch (\Exception $e) {
+					return "系统错误".$e->getMessage();
+				}
 
-	}
+
+			}
 
 	//后台首页
-	public function index(){
-		
+			public function index(){
+
 		//get data
 		// $user = Db::name("users")->select();
-		
+
 		//see   data
 		// var_dump($user);
-		
+
 		// $list = Users::list();
 		// return json($list);
 		//json type
 		// echo $user[0]['username'];
 
 		// return json($user[0]['username']);
-		$teacher = new Teacher();
-		$teacher = $teacher->select();
+				try {
+					$teacher = new Teacher();
+					$teacher = $teacher->select();
 		// var_dump($user);
 		// $js= json($user);
 		// return $user;
 		// var_dump($user);
-		
+
 		// return json($user);
 		// return json($user[0]->getData());
 		// return json($user[0]->getData('username'));
@@ -156,9 +192,13 @@ class IndexController extends Controller
 // $user->where(['username'=>'lyb521569'])->delete();
 
 // return json($users);
-		$this->assign('teacher',$teacher);
-		$html = $this->fetch();
-		return $html;
+					$this->assign('teacher',$teacher);
+					$html = $this->fetch();
+					return $html;
+				} catch (\Exception $e) {
+					return "系统错误".$e->getMessage();
+				}
+
 
 
 		// if(Session::get('userinfo')){
@@ -169,32 +209,32 @@ class IndexController extends Controller
 		// }else{
 		// 	$this->Redirect("/admin/index/login");
 		// }
-	}
+			}
 	//登录
-	public function login()
-	{
+			public function login()
+			{
 		// Session::set("name","lyb");
 		// $name = Session::get("name");
 		// return "somthing".$name;
 
-		return View();
-	}
+				return View();
+			}
 
 
 	//登录动作
-	public function dologin(){
+			public function dologin(){
 
-		if(Request::Instance()->post()){
-			$info = Request::Instance()->post();
+				if(Request::Instance()->post()){
+					$info = Request::Instance()->post();
 			// return json($info);
 			// session_destroy();
-			Session::set("$userinfo",json($info));
+					Session::set("$userinfo",json($info));
 
-			return Redirect("/admin");
-		}else{
-			return redirect("/admin/index/login");
+					return Redirect("/admin");
+				}else{
+					return redirect("/admin/index/login");
+				}
+			}
+
+
 		}
-	}
-
-
-}
